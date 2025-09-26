e2b4898121b097b5cceac9b0a292541306983acf
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-25 23:44:36 -0300

    refactor: melhorar o código

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,41 +1,35 @@
-public class Funcionario{
+public class Funcionario {
     private String nome;
     private String cargo;
     private double salario;
 
-    public Funcionario(String nome, String cargo, double salario){
-        if(salario <= 0){
-            throw new IllegalArgumentException("O salario NAO pode ser menor ou igual a zero!");
-        }
-        this.cargo= cargo;
-        this.nome= nome;
-        this.salario= salario;
+    public Funcionario(String nome, String cargo, double salario) {
+        this.cargo = cargo;
+        this.nome = nome;
+        this.salario = salario;
     }
 
-    public Funcionario(String nome, String cargo){
-        this.nome= nome;
-        this.cargo= cargo;
-        this.salario = 1509.00;
-    }
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public String getCargo(){
+
+    public String getCargo() {
         return cargo;
     }
-    public double getSalario(){
+
+    public double getSalario() {
         return salario;
     }
-    public void setCargo(String cargo){
-        this.cargo= cargo;
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
-    public void setSalario(double salario){
-        if(salario>0){
-            this.salario= salario;
-        }
+
+    public void setSalario(double salario) {
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "%s | %s | R$ %.2f".formatted(nome, cargo, salario);
     }
 }
